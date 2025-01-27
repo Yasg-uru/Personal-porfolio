588e05612a20bbf12c4c1a3669e817c3ba7e1d49
yasg-uru <yashpawar12122004@gmail.com>
2025-01-27 23:49:44 +0530

    corrected small import issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import Details from "./pages/projects/details";
 import RegisterUser from "./pages/auth/registerUser";
 import LoginUser from "./pages/auth/login";
 import { io } from "socket.io-client";
-import HomePage from "./pages/homepage";
 import Header from "./pages/components/header";
 
 import SkillsPage from "./pages/projects/skills";
 import Contact from "./pages/projects/contact";
+import HomePage from "./pages/homepage";
 
 export const socket = io("https://yash-choudhary-portfolio-backend.onrender.com");
 const App: React.FC = () => {
