ae9016127db1b05903c2c0a781fb2aa19b4a1439
yasg-uru <yashpawar12122004@gmail.com>
2025-01-27 23:31:00 +0530

    chancged the axio base url added new deployed url

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Header from "./pages/components/header";
 import SkillsPage from "./pages/projects/skills";
 import Contact from "./pages/projects/contact";
 
-export const socket = io("http://localhost:8001");
+export const socket = io("https://yash-choudhary-portfolio-backend.onrender.com");
 const App: React.FC = () => {
   return (
     <>
